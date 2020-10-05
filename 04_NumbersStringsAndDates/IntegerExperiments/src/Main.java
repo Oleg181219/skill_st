ef3cdf3131bf85803daf57661cda7376dd0e1599
olegprokhorov <igarik10@yandex.ru>
2020-10-05 11:47:53 +0700

    4.1

--- 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
+++ 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
@@ -4,12 +4,32 @@ public class Main
     {
         Container container = new Container();
         container.count += 7843;
-
+        int i = sumDigits(12345);
+        int j = sumDigits(10);
+        int k = sumDigits(5059191);
+        System.out.println("=============================");
+        int i1 = sumDigits2(12345);
+        int j1 = sumDigits2(10);
+        int k1 = sumDigits2(876547865);
     }
 
-    public Integer sumDigits(Integer number)
-    {
-        //@TODO: write code here
-        return 0;
+    public static Integer sumDigits(Integer number) { //публичный, статичный, класс, название метода. ( входные параметры типа Integer)
+        String numbers = Integer.toString(number);   //преобразование в строчный тип.
+        int result = 0;                               // инициализация переменной для суммирования
+        for (int i = 0; i < numbers.length(); i++) {  //цикл для перебора строки по символам. lenght() возвращает количество символов
+            result = result + Integer.parseInt(Character.toString(numbers.charAt(i))); //результат= результат +  Integer.parseInt возврат из числа типа int(возврат строкового объекта (номер позиции в строке))
+        }
+        System.out.println("сумма цифр числа " + number + " = " + result);
+        return result;
+    }
+
+    public static Integer sumDigits2(Integer number) {
+        String numbers = Integer.toString(number);
+        int result = 0;
+        for (int i = 0; i < numbers.length(); i++) {
+            result = result + Integer.parseInt(String.valueOf(numbers.charAt(i)));
+        }
+        System.out.println("сумма цифр числа " + number + " =" + result);
+        return result;
     }
 }
