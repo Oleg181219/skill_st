92c4f1007d7068d3b08de673f8eee878faf0594c
olegprokhorov <igarik10@yandex.ru>
2020-10-05 17:13:40 +0700

    4.2

---
+++ 04_NumbersStringsAndDates/minMax/src/minMax.java
@@ -0,0 +1,19 @@
+public class minMax {
+
+    public static void main(String[] args){
+
+            System.out.println("max int =" + Integer.MAX_VALUE + " min int =" + Integer.MIN_VALUE);
+            System.out.println("max double =" + Double.MAX_VALUE + " min double =" + -Double.MAX_VALUE);
+            System.out.println("max byte =" + Byte.MAX_VALUE + " min byte =" + Byte.MIN_VALUE);
+            System.out.println("max short =" + Short.MAX_VALUE + " min short =" + Short.MIN_VALUE);
+            System.out.println("max long =" + Long.MAX_VALUE + " min long =" + Long.MIN_VALUE);
+            System.out.println("max float =" + Float.MAX_VALUE + " min float =" + -Float.MAX_VALUE);
+            char a = (char)-1;
+            System.out.println("max char " + (int) a + " min char = 0");
+
+
+
+
+        }
+
+}
