0249f4a1e7c0a5a30cfb3b7c515d22179a336f94
olegprokhorov <igarik10@yandex.ru>
2020-10-06 14:05:14 +0700

    4.3 доп исправления

--- 04_NumbersStringsAndDates/Transport/src/Transport.java
+++ 04_NumbersStringsAndDates/Transport/src/Transport.java
@@ -1,14 +1,14 @@
 public class Transport {
 
     public static void main(String[] args) {
-        double numberOfBox = 900;
+        double numberOfBox = 1001;
         int car = 0;
         int cont = 0;
         int box = 0;
-        double c = 0;
+
         double boxOnCar = countingСars.getBoxesInContainer() * countingСars.getContainersInTheTruck();
 
-        for (c = 0; c <= numberOfBox; c++) {
+        for (double c = 0; c <= numberOfBox; c++) {
             if ((c % boxOnCar) == 0) {
                 car++;
                 System.out.println("Грузовик - " + car);
@@ -18,10 +18,12 @@ public class Transport {
                 System.out.println("\t" + "Контейнер - " + cont);
             }
             if (box <= numberOfBox) {
-                box++;
                 System.out.println("\t" + "\t" + "Коробка - " + box);
+                box++;
             }
 
         }
+        System.out.println("Необходимо грузовиков: " + car);
+        System.out.println("Необходимо контейнеров: " + cont);
     }
 }
