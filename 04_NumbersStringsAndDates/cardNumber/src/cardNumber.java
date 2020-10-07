22628070a48b712e1ff9813b8a5ca5079c39cbc9
olegprokhorov <igarik10@yandex.ru>
2020-10-07 18:12:02 +0700

    4.4 доп. вариант

--- 04_NumbersStringsAndDates/cardNumber/src/cardNumber.java
+++ 04_NumbersStringsAndDates/cardNumber/src/cardNumber.java
@@ -6,10 +6,10 @@ public class cardNumber {
     public static void main(String[] args) throws IOException {
 
 
-       /* System.out.println("Введите Фамилию Имя Отчество:");
-        String incomingNumber = ((new BufferedReader(new InputStreamReader(System.in))).readLine()).trim();*/
-        String incomingNumber = "<1234> <7464> 2745 <7468>";
-        //privateCard.searchAndReplaceDiamonds(incomingNumber);
+       System.out.println("Введите номер карты. Выделите знаками < > те цифры которые нужно скрыть");
+        String incomingNumber = ((new BufferedReader(new InputStreamReader(System.in))).readLine()).trim();
+
+
        System.out.println("4-"+privateCard.searchAndReplaceDiamonds(incomingNumber));
     }
 }
--- 04_NumbersStringsAndDates/cardNumber/src/privateCard.java
+++ 04_NumbersStringsAndDates/cardNumber/src/privateCard.java
@@ -1,38 +1,32 @@
 public class privateCard {
 
 
-    /*private static boolean checkInput (String incomingNumber){
-        for ( int i = 0; i <= incomingNumber.length(); i++){
-            if (((int)incomingNumber.charAt(i) >= 48) & ((int)incomingNumber.charAt(i) >= 57)) ||
-            ((int)incomingNumber.charAt(i) >= 32){
 
-            }
-        }
-    }*/
 
     public static String searchAndReplaceDiamonds(String incomingNumber) {
 
 
-        // String outComingNumber = new String();
-        String rep = "***";
-        String transformNumber = incomingNumber;
-        int j = transformNumber.length();
-        System.out.println(incomingNumber);
-        int firstMin = transformNumber.indexOf("<");
-        int firstMax = transformNumber.indexOf(">");
-        String replace = transformNumber.substring(firstMin + 1, firstMax);
-        System.out.println("1-" + replace);
-        incomingNumber = incomingNumber.replace(replace, rep);
-        System.out.println("2-" + incomingNumber);
 
-            if ((incomingNumber.lastIndexOf(">") > firstMax &
-                    ((incomingNumber.lastIndexOf(">")) <= incomingNumber.length()))) {
-                int min = transformNumber.lastIndexOf("<");
-                int max = transformNumber.lastIndexOf(">");
-                replace = transformNumber.substring(min + 1, max);
-                incomingNumber = incomingNumber.replace(replace, rep);
+        String rep = "<***>";
+        String replace = "";
+        String transformNumber = incomingNumber;
+        int lengthIncomingNumber = incomingNumber.length();
+
+        System.out.println("входящее значение   = " + incomingNumber);
+
+
+
+            while (transformNumber.lastIndexOf(">") < lengthIncomingNumber) {
+                if (incomingNumber.lastIndexOf(">") != lengthIncomingNumber) {
+                    int min = transformNumber.indexOf("<");
+                    int max = transformNumber.indexOf(">");
+                    replace = transformNumber.substring(min , max + 1 );
+                    incomingNumber = incomingNumber.replace(replace, rep);
+                    transformNumber = transformNumber.substring(max + 1);
+                    System.out.println("=======" + transformNumber);
+                    System.out.println(incomingNumber);
+                }
             }
-        System.out.println("еще есть");
 
         return incomingNumber;
 
