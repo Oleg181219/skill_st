77b7c43d2fd62c614ff05baa9bc11dbd6deb7a13
olegprokhorov <igarik10@yandex.ru>
2020-10-08 14:45:16 +0700

    4.5 дополнительное

--- 04_NumbersStringsAndDates/cardNumber/src/privateCard.java
+++ 04_NumbersStringsAndDates/cardNumber/src/privateCard.java
@@ -4,17 +4,9 @@ public class privateCard {
 
 
     public static String searchAndReplaceDiamonds(String incomingNumber) {
-        String rep = "***";
-        String replace = "";
-        String transformNumber = incomingNumber;
-        while (transformNumber.lastIndexOf("<") != -1) {
-                    int min = transformNumber.indexOf("<");
-                    int max = transformNumber.indexOf(">");
-                    replace = transformNumber.substring(min , max + 1 );
-                    incomingNumber = incomingNumber.replace(replace, rep);
-                    transformNumber = transformNumber.substring(max + 1);
-            }
-        incomingNumber = incomingNumber.replaceAll("\\s+", "");// убирает пробелы.
+
+        incomingNumber = incomingNumber.replaceAll("<.+?>", "***");
+        incomingNumber = incomingNumber.replaceAll("\\s+", "");
         return incomingNumber;
     }
 
