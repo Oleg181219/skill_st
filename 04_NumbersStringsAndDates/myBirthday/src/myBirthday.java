d27e3aca4b5645e8049bcef1c6e9797cc4739a2d
olegprokhorov <igarik10@yandex.ru>
2020-10-08 23:11:47 +0700

    4.6 сократил немного

--- 04_NumbersStringsAndDates/myBirthday/src/myBirthday.java
+++ 04_NumbersStringsAndDates/myBirthday/src/myBirthday.java
@@ -10,17 +10,13 @@ public class myBirthday {
         int day = 18;
         int month = 12;
         int year = 1977;
-        SimpleDateFormat dateFormat = new SimpleDateFormat(" - dd.MM.yyyy -  EEE", Locale.ENGLISH);// убираем локализацию, получаем на русском
-        Calendar now = Calendar.getInstance();
-        int nowYear = now.get(Calendar.YEAR);
-        System.out.println(nowYear);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat(" - dd.MM.yyyy -  EEE", Locale.ENGLISH);
         Calendar calendar = new GregorianCalendar(year, Calendar.JANUARY, day);
         calendar.set(Calendar.MONTH, month - 1);
-        int i = calendar.get(Calendar.YEAR);
-        while (i <= nowYear) {
-            System.out.println(((i+1) - year - 1) + dateFormat.format((calendar.getTime())));
+        while (calendar.get(Calendar.YEAR) < Calendar.getInstance().get(Calendar.YEAR)) {
+            System.out.println(((calendar.get(Calendar.YEAR)+1) - year - 1) + dateFormat.format((calendar.getTime())));
             calendar.add(Calendar.YEAR, 1);
-            i = calendar.get(Calendar.YEAR);
         }
     }
 }
