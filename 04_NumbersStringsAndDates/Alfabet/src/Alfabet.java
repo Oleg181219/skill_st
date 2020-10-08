ee82575643370474b44bc5385896555079489b7b
olegprokhorov <igarik10@yandex.ru>
2020-10-08 11:39:29 +0700

    задание 4.5.2 исправление

--- 04_NumbersStringsAndDates/Alfabet/src/Alfabet.java
+++ 04_NumbersStringsAndDates/Alfabet/src/Alfabet.java
@@ -3,13 +3,6 @@ import java.util.regex.Pattern;
 public class Alfabet {
 
     public static void main(String[] args) {
-
-       /* String alfabet = "<>";
-        int stringLenght = alfabet.length();
-        for (int i = 0; i < stringLenght; i++) {
-            System.out.println("Буква (" + alfabet.charAt(i) + ") имеет код =" + ((int) (alfabet.charAt(i))));
-        }
-*/
         Pattern p = Pattern.compile("\\s+");
     String news = "Britain’s finance minister has charmed the public with his easy manner and empathy since the " +
             "pandemic plunged the country into crisis in March. Over coffee and croissants in his rooms at No. " +
@@ -17,6 +10,8 @@ public class Alfabet {
             "want him to move next door to take over as prime minister.";
         System.out.println(news);
         System.out.println(news.length());
+        news = news.replaceAll("\\,", " ,");
+        news = news.replaceAll("\\.", " .");
         String[] newString = p.split(news);
         for (int i = 0; i < newString.length; i++){
             System.out.println(newString[i]);
