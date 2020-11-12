accc5616ba20c75236acfb897e4dddb0b18fcfce
olegprokhorov <igarik10@yandex.ru>
2020-11-12 17:22:32 +0700

    9.4 fin

--- 09_FilesAndNetwork/Lenta 9.4/src/Lenta.java
+++ 09_FilesAndNetwork/Lenta 9.4/src/Lenta.java
@@ -1,19 +1,24 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
-import java.util.Arrays;
-import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Lenta {
     private static final String target = "files/";
+    private static Logger rootLogger;
 
     public static void main(String[] args) {
+        rootLogger = LogManager.getRootLogger();
         try {
             Document doc = Jsoup.connect("https://lenta.ru").get();
             Elements links1 = doc.select("img");
@@ -24,26 +29,23 @@ public class Lenta {
                 String string = website.toString();
                 String[] filds = string.split("/");
                 String nameFile = filds[filds.length - 1];
-                String finalTarget = target.concat(nameFile);
-                File newDir =  new File(target);
-                if (!newDir.exists()) newDir.mkdir();
 
-                ReadableByteChannel readableByteChannel = Channels.newChannel(website.openStream());
-                FileOutputStream fileOutputStream = new FileOutputStream(finalTarget);
-                fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
-
+                if (nameFile.matches("([^\\s]+(\\.(?i)(jpg|png|gif|bmp))$)")) {
+                    String finalTarget = target.concat(nameFile);
+                    File newDir = new File(target);
+                    if (!newDir.exists()) newDir.mkdir();
+                    ReadableByteChannel readableByteChannel = Channels.newChannel(website.openStream());
+                    FileOutputStream fileOutputStream = new FileOutputStream(finalTarget);
+                    fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+                }
             }
-            // это Objects.requireNonNull(directory.listFiles()))  IDEA  попросила. Я не стал отказываться.
-            // разбираюсь что это такое.
-            File directory = new File(target);
-            Arrays.stream(Objects.requireNonNull(directory.listFiles()))
-                    .map(File::getName)
-                    .forEach(System.out::println);
-
         } catch (Exception e) {
             e.printStackTrace();
+            rootLogger.error(e.getMessage() + "\n");
         }
 
     }
+
+
 }
 
