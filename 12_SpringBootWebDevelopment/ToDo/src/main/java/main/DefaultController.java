002d8d8dcc5bbeffa99360d86711a5eab1afd3f2
olegprokhorov <igarik10@yandex.ru>
2020-12-24 01:10:04 +0700

    12.1

---
+++ 12_SpringBootWebDevelopment/ToDo/src/main/java/main/DefaultController.java
@@ -0,0 +1,12 @@
+package main;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DefaultController {
+    @RequestMapping("/")
+    public String index() {
+        return String.valueOf(Math.random()*100);
+    }
+}
