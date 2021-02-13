32d87dec5bff806e46bd5a779228422f090b61d5
olegprokhorov <igarik10@yandex.ru>
2021-02-13 23:20:38 +0700

    18.1-3

--- 18_Algorithms/src/main/java/binary_search/BinarySearch.java
+++ 18_Algorithms/src/main/java/binary_search/BinarySearch.java
@@ -18,7 +18,25 @@ public class BinarySearch
 
     private int search(String query, int from, int to)
     {
-        //TODO: write code here
-        return -1;
+        // берем значение в середине
+        int middle = (from + to) / 2;
+        // если инд старта больше окончания то массив пустой. возвращаем -1
+        if (to < from) {
+            return -1;
+        }
+        // берем значение для сравнения, сравнивая строку поиска и значение в середине выборки
+
+        int compare = query.compareTo(list.get(middle));
+        // если compare равен '0' , то объект найден, возвращаем его индекс
+        if (compare == 0) {
+            return middle;
+        }
+        // если compare > '0', то рекурсией запрашиваем search но к from = middle + 1
+        if (compare > 0) {
+            return search(query, middle + 1, to);
+        }
+        // иначе  compare < '0', то рекурсией запрашиваем search но к to = middle - 1
+        return search(query, from, middle - 1);
+
     }
 }
