6c498b0ff8c70fac91d03d05641ddeb0ec8c8877
olegprokhorov <igarik10@yandex.ru>
2021-02-16 13:06:38 +0700

    18.1-6.1

--- 18_Algorithms/src/main/java/merge_sort/MergeSort.java
+++ 18_Algorithms/src/main/java/merge_sort/MergeSort.java
@@ -1,11 +1,9 @@
 package merge_sort;
 
-public class MergeSort
-{
-    public static void mergeSort(int[] array)
-    {
+public class MergeSort {
+    public static void mergeSort(int[] array) {
         int n = array.length;
-        if(n < 2) {
+        if (n < 2) {
             return;
         }
         int middle = n / 2;
@@ -18,14 +16,25 @@ public class MergeSort
         for (int i = middle; i < n; i++) {
             rightArray[i - middle] = array[i];
         }
+
         mergeSort(leftArray);
         mergeSort(rightArray);
 
         merge(array, leftArray, rightArray);
     }
 
-    private static void merge(int[] array, int[] left, int[] right)
-    {
-        //TODO
+    private static void merge(int[] array, int[] left, int[] right) {
+        int leftArrayIndex = 0;
+        int rightArrayIndex = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (rightArrayIndex >= right.length || (leftArrayIndex < left.length && left[leftArrayIndex] <= right[rightArrayIndex])) {
+                array[i] = left[leftArrayIndex];
+                leftArrayIndex++;
+            } else {
+                array[i] = right[rightArrayIndex];
+                rightArrayIndex++;
+            }
+        }
     }
 }
